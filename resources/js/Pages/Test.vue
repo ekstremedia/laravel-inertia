1512ec611fc685c828983e56665ab8b1aec8b8ab
Terje Nesthus <terje@ekstremedia.no>
2021-06-03 11:51:12 +0200

    first commit

---
+++ resources/js/Pages/Test.vue
@@ -0,0 +1,18 @@
+<template>
+<div>
+    TEST
+</div>
+</template>
+
+<script>
+export default {
+    name: "Test",
+    mounted() {
+        console.log('test')
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
